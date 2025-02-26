b38162a53130120037b6042deacd5bc1cb99835f
AuraRenata812 <becaria.aura@mideservices.com>
2025-02-26 02:43:24 -0600

    services listos ambos lados back y front

--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,41 @@
-const admin = require("firebase-admin");
-const {
-  crearServicio,
-  obtenerServicios,
-  actualizarServicio,
-  eliminarServicio,
-} = require("./servicios");
+const functions = require("firebase-functions");
+const cors = require("cors");
+const corsHandler = cors({origin: true});
 
-const {registrarCliente,
-  obtenerClientes,
-  actualizarCliente,
-  eliminarCliente,
-} = require("./clientes");
+const admin = require("firebase-admin");
+
+const servicios = require("./servicios");
+const clientes = require("./clientes");
 
 // Inicializar Firebase Admin solo si no está inicializado
 if (admin.apps.length === 0) {
   admin.initializeApp();
 }
 
-// ✅ Exportamos las funciones de Servicios
-exports.crearServicio = crearServicio;
-exports.obtenerServicios = obtenerServicios;
-exports.actualizarServicio = actualizarServicio;
-exports.eliminarServicio = eliminarServicio;
+// ✅ Wrappers con CORS para cada función de Servicios
+exports.crearServicio = functions.https.onRequest((req, res) =>
+  corsHandler(req, res, () => servicios.crearServicio(req, res)),
+);
+exports.obtenerServicios = functions.https.onRequest((req, res) =>
+  corsHandler(req, res, () => servicios.obtenerServicios(req, res)),
+);
+exports.actualizarServicio = functions.https.onRequest((req, res) =>
+  corsHandler(req, res, () => servicios.actualizarServicio(req, res)),
+);
+exports.eliminarServicio = functions.https.onRequest((req, res) =>
+  corsHandler(req, res, () => servicios.eliminarServicio(req, res)),
+);
 
-// ✅ Exportamos las funciones de Clientes
-exports.registrarCliente = registrarCliente;
-exports.obtenerClientes = obtenerClientes;
-exports.actualizarCliente = actualizarCliente;
-exports.eliminarCliente = eliminarCliente;
+// ✅ Wrappers con CORS para cada función de Clientes
+exports.registrarCliente = functions.https.onRequest((req, res) =>
+  corsHandler(req, res, () => clientes.registrarCliente(req, res)),
+);
+exports.obtenerClientes = functions.https.onRequest((req, res) =>
+  corsHandler(req, res, () => clientes.obtenerClientes(req, res)),
+);
+exports.actualizarCliente = functions.https.onRequest((req, res) =>
+  corsHandler(req, res, () => clientes.actualizarCliente(req, res)),
+);
+exports.eliminarCliente = functions.https.onRequest((req, res) =>
+  corsHandler(req, res, () => clientes.eliminarCliente(req, res)),
+);
