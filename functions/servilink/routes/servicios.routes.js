9e38a758628ec934be1aacfc5b3ce57b233f591f
AuraRenata812 <becaria.aura@mideservices.com>
2025-03-17 16:00:06 -0600

    servicios ready

--- functions/servilink/routes/servicios.routes.js
+++ functions/servilink/routes/servicios.routes.js
@@ -2,19 +2,19 @@ const express = require("express");
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
-const {verificarToken} = require("../../autolavado/middlewares/authMiddleware");
+
 const {
   crearServicio,
   obtenerServicios,
-  obtenerServicioPorId,
+  obtenerServiciosPorProveedor,
   actualizarServicio,
   eliminarServicio,
 } = require("../controllers/servicios");
 
-router.post("/crearservicio", verificarToken, crearServicio);
-router.get("/obtenerservicios", verificarToken, obtenerServicios);
-router.get("/obtenerservicio/:id", verificarToken, obtenerServicioPorId);
-router.put("/editarservicio/:id", verificarToken, actualizarServicio);
-router.delete("/eliminarservicio/:id", verificarToken, eliminarServicio);
+router.post("/crearservicio", crearServicio);
+router.get("/obtenerservicios", obtenerServicios);
+router.get("/obtenerservicios/:idProveedor", obtenerServiciosPorProveedor);
+router.put("/actualizarservicio/:id", actualizarServicio);
+router.delete("/eliminarservicio/:id", eliminarServicio);
 
 module.exports = router;
